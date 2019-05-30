edaac171eb5a6913f872ae43174f7398d5397c19
Daniel Erickson <techwraithpdx@gmail.com>
2019-05-30 16:59:04 -0700

    pared down test

---
+++ api/test-2/index.go
@@ -0,0 +1,12 @@
+package api
+
+import (
+	"net/http"
+	shared "github.com/techwraith/now-go-test-case/shared-package"
+)
+
+// Handler func
+func Handler(w http.ResponseWriter, r *http.Request) {
+	message := shared.ResponseGenerator("test-2")
+	w.Write([]byte(message))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/techwraith/now-go-test-case
+
+go 1.12
